1f3cd095725500a3e8f9278c92a3213ce9a95872
BroingerC <christina.broinger@stud.fh-campuswien.ac.at>
2022-10-04 19:41:03 +0200

    task 3 completed

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -14,15 +14,15 @@ public class App {
     public void helloRobot(){
         // input your solution here
         System.out.println("0123456789012345678901");
-        System.out.println("         __           ");
-        System.out.println(" _(\\    |@@|          ");
-        System.out.println("(__/\\__ \\--/ __       ");
-        System.out.println("   \\___|----|  |   __ ");
+        System.out.println("         __");
+        System.out.println(" _(\\    |@@|");
+        System.out.println("(__/\\__ \\--/ __");
+        System.out.println("   \\___|----|  |   __");
         System.out.println("       \\ }{ /\\ )_ / _\\");
         System.out.println("       /\\__/\\ \\__O (__");
-        System.out.println("      (--/\\--)     \\__/");
-        System.out.println("      _)(  )(_        ");
-        System.out.println("     `---''---`       ");
+        System.out.println("      (--/\\--)    \\__/");
+        System.out.println("      _)(  )(_");
+        System.out.println("     `---''---`");
     }
 
     //todo Task 3
